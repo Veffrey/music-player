66f9ba873d48a6008e49fea6fc8163ee9e47676e
veffrey <veffreyokhamera@gmail.com>
2022-07-04 21:01:49 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+var mySong = document.getElementById("mySong");
+            var icon = document.getElementById("icon");
+
+            function toggleMobileMenu(){
+                menubar.classList.toggle('open');
+            }
+
+            icon.onclick = function(){
+                if (mySong.paused){
+                    mySong.play();
+                    icon.src = "assets/pause.png"
+                }else{
+                    mySong.pause();
+                    icon.src = "assets/play.png";
+                }
+
+            }
+let navLinks = document.getElementById("navLinks")
+    w
+        function showMenu() {
+            navLinks.style.right = "0"
+        }
+        function hideMenu() {
+            navLinks.style.right = "-200px"
+        }
